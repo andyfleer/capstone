38819b694afa9e4a4d4f293283bb95e758529c6e
andyfleer <r-enter@ya.ru>
2022-03-28 03:02:41 +0700

    added our sign in component as well as firebase utils and brought in firebase libraries

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
-import Navigation from './routes/navigation/navigation.component';
-
-
+import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 const Shop = () => <h1>i am the shop</h1>;
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigation />} >
+      <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
